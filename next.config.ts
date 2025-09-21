0544f79b0b47f934264a6adc85f22e3e29d9185f
miever <miever1@163.com>
2025-09-21 20:41:17 +0300

    init project with Next.js, TypeScript, Chakra UI, and ESLint config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'miever.s3.ap-east-1.amazonaws.com',
+        port: '',
+        pathname: '/static/**',
+        search: '',
+      },
+    ],
+    formats: ['image/webp']
+  },
 };
 
 export default nextConfig;
